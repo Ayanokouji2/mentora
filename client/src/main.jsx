6806e88fb59ba7326ab036cca551dba34693e88d
Shivam_Sharma <shivamsharma8434202092@gmail.com>
2025-09-20 23:28:02 +0530

    setup tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom"
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<Router>
+			<Toaster position="top-center" richColors />
+			<App />
+		</Router>
+	</StrictMode>,
 )
