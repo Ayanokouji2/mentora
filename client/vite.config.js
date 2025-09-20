cf4d2b6ebda9ace2c69761c4aa36f8df2bd1cad0
Shivam_Sharma <shivamsharma8434202092@gmail.com>
2025-09-21 00:03:27 +0530

    shadcn-setup complete

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(),tailwindcss(),],
-	server:{
-		host:"0.0.0.0",
-		port: 5173
-	}
+	plugins: [react(), tailwindcss()],
+	server: {
+		host: "0.0.0.0",
+		port: 5173,
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
